8f1f77d784dd763d20c3b0a2ccf9df14c6b56690
Sachin Tewari <mailforsachin@gmail.com>
2023-07-24 19:53:36 -0400

    Update counterscript.js

--- counterscript.js
+++ counterscript.js
@@ -31,7 +31,7 @@ function updateCounter(){
   seconds = seconds < 10 ? '0' + seconds : seconds;
 
   // update the HTML element with the id 'counter' with all of the values
-  document.getElementById('counter').innerHTML = years+' years, '+months+' months, '+days+' days, '+hours+' hours, '+minutes+' minutes, and '+seconds+' seconds';
+  document.getElementById('counter').innerHTML = years+' years, '+months+' months;
 }
 
 // run the updateCounter function every second
